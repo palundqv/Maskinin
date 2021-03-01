2226de1753e622ecea3a6257c87e4060b0484d09
palundqv <ph.lundqvist@gmail.com>
2021-03-01 11:48:50 +0100

    Laddat upp flera olika filer

---
+++ example.py
@@ -0,0 +1,69 @@
+# we can take the next step and improve the model’s generalization 
+# performance by tuning its parame‐ters. s. 261
+"""
+One way to resolve this problem is to split the data again, so we 
+have three sets: the training set to build the model, the validation 
+(or development) set to select the parameters of the model, and 
+the test set to evaluate the performance of the selected parameters.
+"""
+
+from sklearn.svm import SVC
+# split data into train+validation set and test set
+X_trainval, X_test, y_trainval, y_test = train_test_split(
+    iris.data, iris.target, random_state=0)
+# split train+validation set into training and validation sets
+X_train, X_valid, y_train, y_valid = train_test_split(
+    X_trainval, y_trainval, random_state=1)
+print("Size of training set: {} size of validation set: {} size of test set:"
+" {}\n".format(X_train.shape[0], X_valid.shape[0], X_test.shape[0]))
+
+best_score = 0
+
+for gamma in [0.001, 0.01, 0.1, 1, 10, 100]:
+    for C in [0.001, 0.01, 0.1, 1, 10, 100]:
+        # for each combination of parameters, train an SVC
+        svm = SVC(gamma=gamma, C=C)
+        svm.fit(X_train, y_train)
+        # evaluate the SVC on the test set
+        score = svm.score(X_valid, y_valid)
+        # if we got a better score, store the score and parameters
+        if score > best_score:
+        best_score = score
+        best_parameters = {'C': C, 'gamma': gamma}
+
+
+# rebuild a model on the combined training and validation set,
+# and evaluate it on the test set
+svm = SVC(**best_parameters)
+svm.fit(X_trainval, y_trainval)
+test_score = svm.score(X_test, y_test)
+print("Best score on validation set: {:.2f}".format(best_score))
+print("Best parameters: ", best_parameters)
+print("Test set score with best parameters: {:.2f}".format(test_score))
+
+
+
+"""
+For a better estimate of the generalization performance, instead of
+using a single split into a training and a validation set, we 
+can use cross-validation to evaluate the performance of each 
+parameter combination. This method can be coded up as follows:
+"""
+
+for gamma in [0.001, 0.01, 0.1, 1, 10, 100]:
+    for C in [0.001, 0.01, 0.1, 1, 10, 100]:
+        # for each combination of parameters,
+        # train an SVC
+        svm = SVC(gamma=gamma, C=C)
+        # perform cross-validation
+        scores = cross_val_score(svm, X_trainval, y_trainval, cv=5)
+        # compute mean cross-validation accuracy
+        score = np.mean(scores)
+        # if we got a better score, store the score and parameters
+        if score > best_score:
+            best_score = score
+            best_parameters = {'C': C, 'gamma': gamma}
+            # rebuild a model on the combined training and validation set
+svm = SVC(**best_parameters)
+svm.fit(X_trainval, y_trainval)
+# Finns ännu en version på s. 265
