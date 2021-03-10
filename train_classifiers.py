b6db8885fdcdaeaad14f586f799125e10b6578c6
palundqv <ph.lundqvist@gmail.com>
2021-03-10 08:34:02 +0100

    Försöker pusha igen

--- train_classifiers.py
+++ train_classifiers.py
@@ -6,22 +6,17 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.decomposition import PCA
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import GridSearchCV
-import features
-import sys
-import numpy
-numpy.set_printoptions(threshold=sys.maxsize)
-
 
 # KNN Classifier ##############################################################################################################
-def apply_KNeighborsClassifier(X_train_pca, X_test_pca, y_train):
+def apply_KNeighborsClassifier(X_train_pca, X_test_pca, y_train, n_neighbors):
     # https://stackoverflow.com/questions/59830510/defining-distance-parameter-v-in-knn-crossval-grid-search-seuclidean-mahalano
     
     knn = KNeighborsClassifier()
     
     grid_params = [
-        {'n_neighbors': np.arange(1, 51), 'metric': ['euclidean', 'minkowski', 'manhattan']},
+        {'n_neighbors': np.arange(1, 51), 'metric': ['euclidean', 'minkowski']},
         {'n_neighbors': np.arange(1, 51), 'metric': ['mahalanobis', 'seuclidean'],
-        'metric_params': [{'V': np.cov(X_train_pca, rowvar=False)}]}]
+        'metric_params': [{'V': np.cov(X_train_pca)}]}]
 
     knn_gscv = GridSearchCV(estimator=knn, param_grid=grid_params[0], cv=5)
 
@@ -65,20 +60,6 @@ def find_best_interations():
 
 # CNN classifier #######################################################################################################################
 
-# def apply cnn classifier
-
-if __name__ == '__main__':
-    X_train, X_test, y_train, y_test, X, Y = datasetreader.get_dataset(
-        'Sign-Language-Digits-Dataset-master\Dataset')
-
-    n_components = 10
-
-    X_train_pca, X_test_pca = features.apply_PCA(X_train, X_test, n_components)
-
-    y_pred, knn = apply_KNeighborsClassifier(X_train_pca, X_test_pca, y_train)
-
-    print("Test set score: {:.2f}".format(np.mean(y_pred == y_test)))
-    print(knn.best_params_)
 def apply_cnn_classifier():
     pass
 
