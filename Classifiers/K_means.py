19af127903d5d82d56bea02e4a47995c550ebe0e
palundqv <ph.lundqvist@gmail.com>
2021-03-13 15:23:49 +0100

    Skapat K_means filen

---
+++ Classifiers/K_means.py
@@ -0,0 +1,5 @@
+from sklearn.cluster import KMeans
+
+km = KMeans(n_clusters=10, random_state=0)
+labels_km = km.fit_predict(X_pca)
+print("Cluster sizes k-means: {}".format(np.bincount(labels_km)))
