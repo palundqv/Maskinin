61e1ecc48e716aa8a9ce279acf3f468776364468
Amandabru <abrundin@kth.se>
2021-03-14 16:41:04 +0100

    kul med ändringar, bättre beskrivning än poppes ia

--- K_neighbors.py
+++ K_neighbors.py
@@ -1,7 +1,14 @@
 from sklearn.model_selection import GridSearchCV
 from sklearn.neighbors import KNeighborsClassifier
+import numpy as np
+import matplotlib.pyplot as plt
+import PCA
+import datasetreader
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
 
-def apply_KNeighborsClassifier(X_train, X_test, y_train):
+
+def kNN_param(X_train, X_val, y_train):
     # https://stackoverflow.com/questions/59830510/defining-distance-parameter-v-in-knn-crossval-grid-search-seuclidean-mahalano
     # Gridsearch hittar bästa kombinationen av hyperparametrarna k och distance av kNN och predictar på dem.
 
@@ -13,11 +20,16 @@ def apply_KNeighborsClassifier(X_train, X_test, y_train):
     knn_gscv = GridSearchCV(estimator=knn, param_grid=grid_params, cv=5)
     knn_gscv.fit(X_train, y_train)
 
-    y_pred = knn_gscv.predict(X_test)
+    y_pred = knn_gscv.predict(X_val)
     
     return y_pred, knn_gscv
 
-    
+
+def apply_knn_classifier(X_train_pca, X_test_pca, y_train, neighbors, distance):
+    clf = KNeighborsClassifier(n_neighbors=neighbors, metric=distance).fit(X_train_pca, y_train)
+    knn_predicts = clf.predict(X_test_pca)
+    return knn_predicts
+
 
 def Kneighbors_plotter(n_neighbors, X_train_pca, y_train, X_test_pca, y_test):
     # Plottar accuracy mot antalet k i kNN.
@@ -35,5 +47,34 @@ def Kneighbors_plotter(n_neighbors, X_train_pca, y_train, X_test_pca, y_test):
     plt.xlabel("n_neighbors")
     plt.legend()
     plt.grid()
-    plt.show()
+    plt.show()   
 
+
+def evaluate(y_test, y_pred):
+    # printar ut tabell med precision, recall, accuracy och f-measure
+    target_names = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    print(classification_report(y_test, y_pred,target_names=target_names))
+
+
+if __name__ == '__main__':
+    X_trainval, X_test, y_trainval, y_test, X, Y = datasetreader.get_dataset(
+        'Sign-Language-Digits-Dataset-master\Dataset')
+
+    X_train, X_val, y_train, y_val = train_test_split(X_trainval, y_trainval, test_size=0.5, random_state=0)
+
+    #lista = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+    #for i in lista:
+    X_train_pca, X_val_pca, pca = PCA.apply_PCA(X_train, X_val, 0.6)
+
+    y_pred, knn = kNN_param(X_train_pca, X_val_pca, y_train)
+    print("Test set score: {:.2f}".format(np.mean(y_pred == y_val)))
+    print(knn.best_params_)
+
+    X_trainval_pca, X_test_pca, pca = PCA.apply_PCA(X_trainval, X_test, 0.6)
+    y_predict = apply_knn_classifier(X_trainval_pca, X_test_pca, y_trainval, 
+    knn.best_params_['n_neighbors'], knn.best_params_['metric'])
+    evaluate(y_test, y_predict)
+
+
+
+    
